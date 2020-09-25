075b6651018582fb18ee2b1a107f90c5807785ef
mat <lishay.matvey20001506matlish>
2020-09-25 20:31:06 +0300

    (Project) Languages Nav

--- es-react/app/components/Popular.js
+++ es-react/app/components/Popular.js
@@ -1,5 +1,22 @@
 import React from 'react'
 
+function LanguagesNav({selected, onUpdateLanguage}) {
+    const languages = ['All', 'JavaScript', 'Java', 'Css', 'Python']
+    return (
+        <ul className='nav'>
+            {languages.map((item) => (
+                <li key={item} className='nav__item'>
+                    <button className='nav__btn'
+                            style={item === selected ? {color: '#ff4d96'} : null}
+                            onClick={() => onUpdateLanguage(item)}>
+                        {item}
+                    </button>
+                </li>
+            ))}
+        </ul>
+    )
+}
+
 export default class Popular extends React.Component {
     constructor(props) {
         super(props);
@@ -11,25 +28,22 @@ export default class Popular extends React.Component {
     }
 
     updateLanguage(selectedLanguage) {
+        console.log(this.state);
         this.setState({
             selectedLanguage
         })
     }
 
     render() {
-        const languages = ['All', 'JavaScript', 'Java', 'Css', 'Python']
+        const {selectedLanguage} = this.state
+
         return (
-            <ul className='nav'>
-                {languages.map((item) => (
-                    <li key={item} className='nav__item'>
-                        <button className='nav__btn'
-                                style={item === this.state.selectedLanguage ? {color: '#ff4d96'} : null}
-                                onClick={() => this.updateLanguage(item)}>
-                            {item}
-                        </button>
-                    </li>
-                ))}
-            </ul>
+            <React.Fragment>
+                <LanguagesNav
+                    selected={selectedLanguage}
+                    onUpdateLanguage={this.updateLanguage}
+                />
+            </React.Fragment>
         )
     }
 }
